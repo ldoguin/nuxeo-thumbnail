8f80054ee83011bce63d9f97ae8bcfa71fe3a7ff
Laurent Doguin <ldoguin@nuxeo.com>
2012-06-05 15:18:39 +0200

    make the listener asynchronous

--- src/main/java/org/nuxeo/thumb/listener/UpdateThumbListener.java
+++ src/main/java/org/nuxeo/thumb/listener/UpdateThumbListener.java
@@ -14,12 +14,14 @@ package org.nuxeo.thumb.listener;
 
 import org.nuxeo.ecm.core.api.Blob;
 import org.nuxeo.ecm.core.api.ClientException;
+import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.blobholder.BlobHolder;
 import org.nuxeo.ecm.core.api.event.DocumentEventTypes;
 import org.nuxeo.ecm.core.event.Event;
+import org.nuxeo.ecm.core.event.EventBundle;
 import org.nuxeo.ecm.core.event.EventContext;
-import org.nuxeo.ecm.core.event.EventListener;
+import org.nuxeo.ecm.core.event.PostCommitEventListener;
 import org.nuxeo.ecm.core.event.impl.DocumentEventContext;
 import org.nuxeo.thumb.AddThumbnailUnrestricted;
 import org.nuxeo.thumb.ThumbnailConstants;
@@ -27,35 +29,53 @@ import org.nuxeo.thumb.ThumbnailConstants;
 /**
  * @author ldoguin
  */
-public class UpdateThumbListener implements EventListener {
+public class UpdateThumbListener implements PostCommitEventListener {
 
 	public void handleEvent(Event event) throws ClientException {
 		EventContext ec = event.getContext();
 		if (ec instanceof DocumentEventContext) {
 			DocumentEventContext context = (DocumentEventContext) ec;
 			DocumentModel doc = context.getSourceDocument();
-			if (doc.isDirty() || DocumentEventTypes.DOCUMENT_CREATED.equals(event.getName())) {
-				BlobHolder blobHolder = doc.getAdapter(BlobHolder.class);
-				if (blobHolder != null) {
-					Blob blob = blobHolder.getBlob();
-					if (blob != null) {
-						try {
-							AddThumbnailUnrestricted runner = new AddThumbnailUnrestricted(
-									context.getCoreSession(), doc, blobHolder);
-							runner.runUnrestricted();
-							return;
-						} catch (Exception e) {
-							throw new RuntimeException(e);
-						}
+			BlobHolder blobHolder = doc.getAdapter(BlobHolder.class);
+			if (blobHolder != null) {
+				Blob blob = blobHolder.getBlob();
+				if (blob != null) {
+					try {
+						AddThumbnailUnrestricted runner = new AddThumbnailUnrestricted(
+								context.getCoreSession(), doc, blobHolder);
+						runner.run();
+						context.getCoreSession().save();
+						return;
+					} catch (Exception e) {
+						throw new RuntimeException(e);
 					}
 				}
-				// No Blob anymore, remove the facet
-				if (doc.hasFacet(ThumbnailConstants.THUMBNAIL_FACET)) {
-					doc.removeFacet(ThumbnailConstants.THUMBNAIL_FACET);
-					context.getCoreSession().saveDocument(doc);
-				}
+			}
+			// No Blob anymore, remove the facet
+			if (doc.hasFacet(ThumbnailConstants.THUMBNAIL_FACET)) {
+				doc.removeFacet(ThumbnailConstants.THUMBNAIL_FACET);
+				CoreSession coreSession = context.getCoreSession();
+				coreSession.saveDocument(doc);
+				coreSession.save();
 			}
 		}
+	}
+
+	@Override
+	public void handleEvent(EventBundle events) throws ClientException {
+		if (!events.containsEventName(DocumentEventTypes.DOCUMENT_CREATED)
+				&& !events
+						.containsEventName(DocumentEventTypes.DOCUMENT_UPDATED)) {
+			return;
+		}
+		for (Event event : events) {
+			if (DocumentEventTypes.DOCUMENT_CREATED.equals(event.getName())
+					|| DocumentEventTypes.DOCUMENT_UPDATED.equals(event
+							.getName())) {
+				handleEvent(event);
+			}
+		}
+
 	}
 
 }
--- src/test/java/org/nuxeo/thumb/test/TestThumbnail.java
+++ src/test/java/org/nuxeo/thumb/test/TestThumbnail.java
@@ -24,12 +24,16 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.nuxeo.common.utils.FileUtils;
 import org.nuxeo.ecm.core.api.Blob;
+import org.nuxeo.ecm.core.api.ClientException;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.impl.blob.FileBlob;
+import org.nuxeo.ecm.core.event.EventService;
+import org.nuxeo.ecm.core.event.EventServiceAdmin;
 import org.nuxeo.ecm.core.test.CoreFeature;
 import org.nuxeo.ecm.core.test.annotations.BackendType;
 import org.nuxeo.ecm.core.test.annotations.RepositoryConfig;
+import org.nuxeo.runtime.api.Framework;
 import org.nuxeo.runtime.test.runner.Deploy;
 import org.nuxeo.runtime.test.runner.Features;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
@@ -61,13 +65,11 @@ public class TestThumbnail {
 		Blob blob = new FileBlob(file);
 		doc.setPropertyValue("file:content", (Serializable) blob);
 		doc = session.createDocument(doc);
-		doc = session.saveDocument(doc);
 		session.save();
+        waitForAsyncCompletion();
+        doc = session.getDocument(doc.getRef());
 		Thumbnail thumb = doc.getAdapter(Thumbnail.class);
 		assertNotNull(thumb);
-		session.save();
-		doc = session.getDocument(doc.getRef());
-		thumb = doc.getAdapter(Thumbnail.class);
 		String firstBlobDigest = thumb.getDigest();
 		Blob thumbnail = thumb.getThumbnail();
 		assertNotNull(thumbnail);
@@ -79,6 +81,9 @@ public class TestThumbnail {
 		blob = new FileBlob(file);
 		doc.setPropertyValue("file:content", (Serializable) blob);
 		doc = session.saveDocument(doc);
+		session.save();
+        waitForAsyncCompletion();
+        doc = session.getDocument(doc.getRef());
 		thumb = doc.getAdapter(Thumbnail.class);
 		String secondBlobDigest = thumb.getDigest();
 		assertNotSame(firstBlobDigest, secondBlobDigest);
@@ -86,8 +91,15 @@ public class TestThumbnail {
 		// remove content
 		doc.setPropertyValue("file:content", null);
 		doc = session.saveDocument(doc);
+		session.save();
+        waitForAsyncCompletion();
+        doc = session.getDocument(doc.getRef());
 		assertFalse(doc.hasFacet(ThumbnailConstants.THUMBNAIL_FACET));
 
 	}
 
+	private void waitForAsyncCompletion() throws ClientException {
+        Framework.getLocalService(EventService.class).waitForAsyncCompletion();
+        session.save(); 
+	}
 }
